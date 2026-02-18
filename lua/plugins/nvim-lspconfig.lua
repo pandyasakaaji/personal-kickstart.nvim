9becc4271730abb2deb0849a1f45f1959a92b377
Saka <pandyasakaaji@gmail.com>
2026-02-19 02:18:05 +0700

    fix(lsp): fix Javascript LSP setup

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -214,6 +214,10 @@ return {
       --
       -- But for many setups, the LSP (`ts_ls`) will work just fine
       ts_ls = {},
+      emmet_ls = {
+        filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css' },
+      },
+
       --
 
       lua_ls = {
